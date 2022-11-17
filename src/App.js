54868e8e01ed4d3c427cc7ab5991f685629908ba
Bartosz Borzęcki <bartosz.borzecki96@gmail.com>
2022-11-17 18:34:49 +0100

    tasks & hideDoneTasks added, js-classes deleted

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+const tasks = [
+  { id: 1, content: "zjeść obiad", done: false },
+  { id: 2, content: "pójść na spacer", done: true },
+];
+
+const hideDoneTasks = false;
+
 
 function App() {
   return (
@@ -7,17 +14,17 @@ function App() {
       </header>
       <section className="section">
         <h2 className="section__header">Dodaj nowe zadanie</h2>
-        <form className="form js-form">
-          <input className="form__input js-newTask" autofocus required placeholder="Co jest do zrobienia?" />
-            <button className="form__button">Dodaj zadanie</button>
+        <form className="form">
+          <input className="form__input" autofocus required placeholder="Co jest do zrobienia?" />
+          <button className="form__button">Dodaj zadanie</button>
         </form>
       </section>
       <section className="section">
         <div className="section__container">
           <h2 className="section__header">Lista zadań</h2>
-          <div className="listButtons js-listButtons"></div>
+          <div className="listButtons"></div>
         </div>
-        <ul className="list js-list"></ul>
+        <ul className="list"></ul>
       </section>
     </main>
   );
