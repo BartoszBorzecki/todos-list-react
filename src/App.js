985dd345eaad9213ea460c15d49219a23780eaaf
Bartosz Borzęcki <bartosz.borzecki96@gmail.com>
2022-11-17 18:29:00 +0100

    html added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
-
 function App() {
   return (
-    <div>
-      Dzień dobry!
-    </div>
+    <main>
+      <header className="header">
+        <h1>Lista zadań</h1>
+      </header>
+      <section className="section">
+        <h2 className="section__header">Dodaj nowe zadanie</h2>
+        <form className="form js-form">
+          <input className="form__input js-newTask" autofocus required placeholder="Co jest do zrobienia?" />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+      </section>
+      <section className="section">
+        <div className="section__container">
+          <h2 className="section__header">Lista zadań</h2>
+          <div className="listButtons js-listButtons"></div>
+        </div>
+        <ul className="list js-list"></ul>
+      </section>
+    </main>
   );
 }
 
