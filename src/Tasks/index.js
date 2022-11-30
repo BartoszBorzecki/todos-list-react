353b56e7cb4fb9c4cfd49ed36fe9ba5e9f9010f1
Bartosz Borzęcki <bartosz.borzecki96@gmail.com>
2022-11-30 17:23:44 +0100

    bug in Tasks component fixed

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -5,12 +5,12 @@ const Tasks = ({tasks, hideDone}) => (
         {tasks.map(task => (
             <li
                 key={task.id}
-                className={'taskList {hideDone && task.done ? "taskList--hide" : ""}'}
+                className={`taskList${hideDone && task.done ? "taskList--hide" : ""}`}
             >
                 <button className="taskList__button">
                     {task.done ? "✔" : ""}
                 </button>
-                <span className={`{task.done ? "taskList__content--done" : ""}`}>{task.content}</span>
+                <span className={`${task.done ? "taskList__content--done" : ""}`}>{task.content}</span>
                 <button className="taskList__button taskList__button--remove"> 🗑 </button>
             </li>
         ))}
